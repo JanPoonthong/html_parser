c76845c496229527b041a21acf69f6c445a6b1ba
Jan Poonthong <janpoonthong628@gmail.com>
2021-07-30 00:37:35 +0700

    Add README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# Html Parser
+
+# Quick Start
+
+## Linux
+
+```console
+$ pip3 install requests
+$ python3 main.py
+```
--- main.py
+++ main.py
@@ -4,10 +4,17 @@ import re
 def main():
     webpage = requests.get("https://janpoonthong.github.io/portfolio/")
     webpage = webpage.text
+    print(webpage)
     extracting(webpage)
 
 def extracting(webpage):
     print(f"Title: {title_tag(webpage)[0]}")
+    print(f"Paragraph: {p_tag(webpage)}")
+
+def p_tag(webpage):
+    #  <\s*p.*?>(.|\s*?)<\/p>
+    # <\s*p.*?>(.|\s*?)<\/p>
+    return re.findall(r'<\s*p.*?>(.*?)<\/p>', webpage)
 
 def title_tag(webpage):
     return re.findall(r'<\s*title.*?>(.*?)<\/title>', webpage)
