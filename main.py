10eb50217c4fa5d11a807073469c8ad0cdc311d1
Jan Poonthong <janpoonthong628@gmail.com>
2021-10-09 21:27:00 +0700

    Format code with black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import requests 
+import requests
 import re
+
 
 def main():
     webpage = requests.get("https://janpoonthong.github.io/portfolio/")
@@ -7,16 +8,20 @@ def main():
     print(webpage)
     extracting(webpage)
 
+
 def extracting(webpage):
     print(f"Title: {title_tag(webpage)[0]}")
     print(f"Paragraph: {p_tag(webpage)}")
 
+
 def p_tag(webpage):
     #  <\s*p.*?>(.|\s*?)<\/p>
     # <\s*p.*?>(.|\s*?)<\/p>
-    return re.findall(r'<\s*p.*?>(.*?)<\/p>', webpage)
+    return re.findall(r"<\s*p.*?>(.*?)<\/p>", webpage)
+
 
 def title_tag(webpage):
-    return re.findall(r'<\s*title.*?>(.*?)<\/title>', webpage)
+    return re.findall(r"<\s*title.*?>(.*?)<\/title>", webpage)
+
 
 main()
